060cb81d17b5752baae4b20c577df0c79af60582
Abhishek Verma <131652685+Abhishek7242@users.noreply.github.com>
2024-06-05 11:36:59 +0530

    Reuploaded
    added new profile image remove junk files

---
+++ web.js
@@ -0,0 +1,36 @@
+let container = document.getElementById('container');
+let home = document.getElementById('home');
+let i = 1;
+home.style.setProperty('--background-url', "url('" + 0 + ".jpg')");
+home.classList.add('home');
+setInterval(() => {
+  if (i == 5) {
+    i = 1;
+  }
+  
+  console.log(i);
+  
+  home.style.setProperty('--background-url', "url('" + i + ".jpg')");
+  home.classList.add('home');
+
+  i++;
+}, 10000);
+
+
+let hamBtn = document.getElementById('hamBtn')
+let potraitNav = document.getElementById('potraitNav')
+hamBtn.addEventListener('click', () => {
+  potraitNav.style.display = 'flex'
+  setTimeout(() => {
+    potraitNav.style.left='0px'
+
+  }, 100);
+})
+let cancelHam = document.getElementById('cancelHam')
+cancelHam.addEventListener('click', () => {
+  potraitNav.style.left = '-400px'
+  setTimeout(() => {
+    
+    potraitNav.style.display='none'
+  }, 200);
+})
